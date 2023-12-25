433b7fa9002080f00c9b9520d3fde29b61d778fe
ANTJNSX <Antombomben@gmail.com>
2023-12-25 02:17:10 +0100

    added full free bst mem function & instert node.

--- program.c
+++ program.c
@@ -28,28 +28,57 @@ node *createNode(int value) {
 }
 
 node *insertNode(node *root, node *newNode) {
-    if ((root != NULL) || (newNode->value == root->value)) {
-       return root; // handle root before adding 
+    if ((root != NULL) && (newNode != NULL)) { // check if root 
+        int rootVal = root->value;
+        int nodeVal = newNode->value;
+        if (rootVal == newNode->value) {
+            return root; // handle root before adding 
+        }
+        
+        if (nodeVal > rootVal) { // check right
+            if (root->right == NULL) {
+                root->right = newNode; 
+                return newNode->value;
+            }
+            instertNode(newNode, root->right);
+        }
+
+        if (nodeVal < rootVal) { // check left
+            if (root->left == NULL) {
+                root->left = newNode; 
+                return newNode->value;
+            }
+            instertNode(newNode, root->left);
+        }
+
     }
 
     return newNode;
 }
 
+void freeBST(node* root) {
+    if (root == NULL) {
+        return;
+    }
+
+    freeBST(root->left);
+    freeBST(root->right);
+    free(root);
+}
+        
+
 int main() {
     node *root = createNode(10); // initialize tree basically
-    
+ 
     printf("%d\n", root->value);
    
     node *rootCh = createNode(5);
 
-    printf("%d\n", rootCh->value);
-
-    root->left = rootCh; 
+    insertNode(root, rootCh);
 
     printf("%d\n", root->left->value);
 
-    free(rootCh);
-    free(root);
+    freeBST(root);
     return 0;
 }
 
